104f3be2207b9bcf015b75ec46f47e3f7d831d86
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-03 11:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function wakeDog(dogName, dogBreed) {
 function leashDog(dogName, dogBreed) {
   return `Leash ${dogName} the ${dogBreed}`;}
 function walkToPark(dogName, dogBreed) {
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);}
+  return `Walk to the park with ${dogName} the ${dogBreed}`;}
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);}
+  return`Throw the frisbee for ${dogName} the ${dogBreed}`;}
 function walkHome(dogName, dogBreed) {
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);}
+  return`Walk home with ${dogName} the ${dogBreed}`;}
 function unleashDog(dogName, dogBreed) {
-  console.log(`Unleash ${dogName} the ${dogBreed}`);}
+  return`Unleash ${dogName} the ${dogBreed}`;}
   const a = "Xerox";
   const b = "Border Collie";
 wakeDog(a,b);
