b2b23078976bcd68d76312080fbc1da5bc85cee9
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-03 11:23:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,45 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);}
+function leashDog(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);}
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);}
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);}
+function walkHome(dogName, dogBreed) {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);}
+function unleashDog(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);}
+  const a = "Xerox";
+  const b = "Border Collie";
+wakeDog(a,b);
+const aa = "Boo Radley";
+  const bb = "Pibble";
+  leashDog(aa, bb);
+const a1 = "Bunny";
+  const b1 = "Labrador";
+  walkToPark(a1, b1);
+const a2 = "Mary J. Blige";
+  const b2 = "Papillon";
+throwFrisbee(a2, b2);
+const a3 = "Santa Paws";
+  const b3 = "Husky";
+walkHome(a3, b3);
+const a4 = "Lauren";
+  const b4 = "Golden Retriever";
+unleashDog(a4, b4);
+const a5 = "Skippy";
+  const b5 = "Welsh Corgi";
+wakeDog(a5, b5);
+function routine(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+
+  console.log(`Unleash ${dogName} the ${dogBreed}`);}
