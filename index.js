2aa27ee0c379a274e2dcbe26377031db0d37490d
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-03 11:38:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let  unleashDog=function(dogName="Byron", dogBreed="poodle") {
   return`Unleash ${dogName} the ${dogBreed}`;}
 let routine=[wakedog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
 
-function routine(dogName="Byron", dogBreed="poodle") {
+function routine(dogName, dogBreed) {
 return routine.map(item=>item(dogName,dogBreed));
 
 }
