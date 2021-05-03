078eca4b9a4bc14c0724ce29b2ee397f10bbb741
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-03 11:27:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function wakeDog(dogName, dogBreed) {
   return `Wake ${dogName} the ${dogBreed}`;
 }
 function leashDog(dogName, dogBreed) {
-  console.log(`Leash ${dogName} the ${dogBreed}`);}
+  return `Leash ${dogName} the ${dogBreed}`;}
 function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);}
 function throwFrisbee(dogName, dogBreed) {
